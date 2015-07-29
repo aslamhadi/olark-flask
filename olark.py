ac27f95e84ddf1c98706c655ee3c885d52cd9d58
Aslam Hadi <aslamhadi@gmail.com>
2015-07-29 16:48:05 +0700

    olarky

--- olark.py
+++ olark.py
@@ -1,8 +1,21 @@
 import os
-from flask import Flask
+import time
+import simplejson
+from pprint import pprint
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/test')
 def hello():
     return 'Hello World!'
+
+
+@app.route("/", methods=["POST"])
+def index():
+    data = simplejson.loads(request.form['data'])
+    print data
+    return jsonify(integrationUrl="http://test.com/%s/" % data['id'])
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=8001)
